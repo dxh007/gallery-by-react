6675238936c0e1ef33799e3488e9073c8ab15a7a
MR DU <d_xinhao@163.com>
2017-01-05 14:34:10 +0800

    构建单个图片显示

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -15,7 +15,6 @@ imageDatas=(function genImageURL(imageDatasArr) {
   console.log(imageDatas);
   for (var i = 0, j = imageDatasArr.length; i < j; i++) {
     var singleImageData = imageDatasArr[i];
-
      singleImageData.imageURL = require('../images/' + singleImageData.fileName);
     imageDatasArr[i] = singleImageData;
   }
@@ -23,13 +22,34 @@ imageDatas=(function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 
+var ImgFigure = React.createClass({
+  render: function(){
+    return (
+        <figure>
+          <img src={this.props.data.imageURL} 
+               alt={this.props.data.title}
+          />
+          <figcaption>
+            <h2>{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+      );
+  }
+});
 var GalleryByReact = React.createClass({
   render:function(){
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigure data={value}/>);
+    });
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     );
